3308f0c5b7489086c2796a23ccedbbf763138423
unknown <>
2021-12-02 13:03:56 +0530

    [udit] resultindictionary

--- arithmeticComputation.sh
+++ arithmeticComputation.sh
@@ -5,7 +5,12 @@ read num2
 echo "Enter c"
 read num3
 
-echo " num1 + num2 * num3= $((num1 + num2 * num3))"
-echo " num1 * num2 + num3= $((num1 * num2 + num3))"
-echo " num3 + num1 / num2= $((num3 + num1 / num2))"
-echo " num1 % num2 / num3= $((num1 % num2 + num3))"
+declare -A expression
+
+expression[first]=`echo " num1 + num2 * num3= $((num1 + num2 * num3))"`
+expression[second]=`echo " num1 * num2 + num3= $((num1 * num2 + num3))"`
+expression[third]=`echo " num3 + num1 / num2= $((num3 + num1 / num2))"`
+expression[fourth]=`echo " num1 % num2 / num3= $((num1 % num2 + num3))"`
+
+echo "value in dictionary " ${expression[@]}
+
